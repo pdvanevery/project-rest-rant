8a20904f39a63e18949be744e4aeeab6da1e874b
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-09 19:45:39 -0800

    added places view w mock data

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,20 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
-    res.render('places/index')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      res.render('places/index', { places })
 })
 
 

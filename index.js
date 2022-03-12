3c2878e5b2164bb9849a006dc143148ba8be9fc1
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-12 12:16:12 -0800

    updated the middleware to encrypt the data

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 
 // Controllers & Routes
-app.use('/places', require('./controllers/places'))
 app.use(express.urlencoded({ extended: true }))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')

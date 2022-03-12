de789196105140c9abcdddd3974efbbe33559a62
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-12 12:29:40 -0800

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
   console.log(req.body)
@@ -7,25 +12,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    //default image if one isnt provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'Pizza Pi',
-        city: 'San Jose',
-        state: 'CA',
-        cuisines: 'Italian',
-        pic: '/images/pizza.jpg'
-      }, {
-        name: 'JavaBean',
-        city: 'San Mateo',
-        state: 'CA',
-        cuisines: 'Coffee',
-        pic: '/images/coffee-bean.jpg'
-      }]
-      res.render('places/index', { places })
-})
+
 
 
 
---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'Pizza Pi',
+    city: 'San Jose',
+    state: 'CA',
+    cuisines: 'Italian',
+    pic: '/images/pizza.jpg'
+  }, {
+    name: 'JavaBean',
+    city: 'San Mateo',
+    state: 'CA',
+    cuisines: 'Coffee',
+    pic: '/images/coffee-bean.jpg'
+  }]
+  

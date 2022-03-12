94502b8c5a685621d96c858807aa1134cdfecf9d
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-12 13:10:44 -0800

    updated the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,17 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name}</h1>
+            <h2>Rating</h2>
+            <p>not rated</p>
+            <h3>Comments</h3>
+          <p>No Comments Yet!</p>
           </main>
+         
         </Def>
     )
 }

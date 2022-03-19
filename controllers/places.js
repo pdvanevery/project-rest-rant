5d5a51d3a04213e9b81b5d9530f7a9b5c48784dd
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-18 18:33:40 -0700

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -19,7 +19,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 })
 
@@ -39,8 +39,19 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
-
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,26 @@ function show (data) {
     return (
         <Def>
           <main>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            Edit
+          </a>
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">
+              Delete
+            </button>
+          </form>  
             <h1>{ data.place.name}</h1>
             <h2>Rating</h2>
             <p>not rated</p>
             <h3>Comments</h3>
           <p>No Comments Yet!</p>
           </main>
-         
+              
         </Def>
     )
+    
 }
+
+
 
 module.exports = show

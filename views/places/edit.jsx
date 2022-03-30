8333a7385fbab5ccc474f27f6a9cbc73de636aba
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-29 20:28:11 -0700

    updated edit and delete routes, they do not work as of now

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -7,27 +7,40 @@ function edit_form (data) {
                 <main>
                     <h1>Edit Place</h1>
                     <form method="POST" action={`/places/${data.id}?_method=PUT`}>
-                    <div className='form-group'>
-                        <label htmlFor="name">Place Name</label>
-                        <input className='form-control' id="name" name="name" defaultValue={data.place.name} required />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className='form-control' id="pic" name="pic" />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="city">City</label>
-                        <input className='form-control' id="city" name="city" />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="state">State</label>
-                        <input className='form-control' id="state" name="state" />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className='form-control' id="cuisines" name="cuisines" required />
-                    </div>
-                    <input className='btn btn-primary' type="submit" value="Add Place" />
+                <div className='row d-flex justify-content-center'>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                <div className='form-group'>
+                    <label htmlFor="name">Place Name</label>
+                    <input className='form-control' id="name" name="name" defaultValue={data.place.name} required  />
+                </div>
+                <div className="form-group">
+                    <label for="founded">Founded Year</label>
+                    <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
+                </div>
+                </div>
+                <div className='col-sm-6 col-md-4 col-lg-3'>
+                <div className='form-group'>
+                    <label htmlFor="pic">Place Picture</label>
+                    <input className='form-control' id="pic" name="pic" />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="city">City</label>
+                    <input className='form-control' id="city" name="city" />
+                </div>
+                </div>
+                <div className='col-sm-6 col-md-4 col-lg-3'>
+                <div className='form-group'>
+                    <label htmlFor="state">State</label>
+                    <input className='form-control' id="state" name="state" />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className='form-control' id="cuisines" name="cuisines" defaultValue={data.place.cuisines}required />
+                </div>
+                </div>
+                </div>
+                <br />
+                    <input className='btn btn-primary' type="submit" value="Update Place" />
                     </form>`
                 </main>
             </Def>
@@ -37,5 +50,7 @@ function edit_form (data) {
 
 module.exports = edit_form
 
+
+                
 
  
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -66,7 +66,7 @@ function show (data) {
           <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
             Edit
           </a>
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+          <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
             <button type="submit" className="btn btn-danger">
               Delete
             </button>

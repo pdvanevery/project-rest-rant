b8772d00212383b7bb8e04c33dae79b6229e29cd
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-29 20:13:36 -0700

    updated the ratings to display in stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,21 +7,33 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let stars= ''
   let rating = (
-    <h3 className='inactive'>
-      Not yet rated
+    <h3>
+      {stars} stars
     </h3>
   )
   if (data.place.comments.length) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
       return tot + c.stars
     }, 0)
-    let averageRating = sumRatings / data.place.comments.length
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
     rating = (
       <h3>
-        {Math.round(averageRating)} stars
+        {stars} stars
       </h3>
     )
+  
+    // sumRatings / data.place.comments.length
+    // rating = (
+    //   <h3>
+    //     {Math.round(averageRating)} stars
+    //   </h3>)
+    
     comments = data.place.comments.map(c => {
       return (
         <div className='border'>

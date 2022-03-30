8d616ed265f9d3625ae6ab587585c2b3102db819
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-03-30 11:55:30 -0700

    updated the edit and delete routes and they fully work now

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
             <Def>
                 <main>
                     <h1>Edit Place</h1>
-                    <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                    <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                 <div className='row d-flex justify-content-center'>
                     <div className='col-sm-6 col-md-4 col-lg-3'>
                 <div className='form-group'>
